a41c3371b3adc125c6dd731769ed024279034e5b
Xergius <castrs01@myunitec.ac.nz>
2022-07-09 15:16:22 +1200

    Hiding things

--- wordsApp/settings.py
+++ wordsApp/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-@i$i!41o_=*nk-3auym3t@%%t$z0nqq1*m1g=z6d2&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
-# DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
